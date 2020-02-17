977e9050caa9f02bcc214f293f185138f72b03f0
XuGuangnian <2431244w@student.gla.ac.uk>
2020-02-17 22:16:32 +0000

    Chapter 4 and exercises completed, test passed.

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render  # you will use it later on
+# chapter 3 exercises
+
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    # return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    # context_dict = {'boldmessage': 'This tutorial has been put together by Gengen Wang!'}
+    return render(request, 'rango/about.html')
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,13 +14,16 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '80w5(@b#ga!!a((w!pgvhkbqbau$ebo=d(pn&w%9s&aq(7j=nc'
+SECRET_KEY = '(4a)2y$ck1tc+@20)819gamt^0%)^lj6k#*gxutya9rk!*t&(_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -119,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
+
